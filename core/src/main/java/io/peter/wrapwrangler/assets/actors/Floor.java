576507e24b48d1dd5b6eb19d2b7cb0bd9871dd84
Abulaf1a <peterrazzell688@gmail.com>
2025-01-03 10:33:11 +0000

    fix: floor dimensions based on texture rather than magic number

--- core/src/main/java/io/peter/wrapwrangler/assets/actors/Floor.java
+++ core/src/main/java/io/peter/wrapwrangler/assets/actors/Floor.java
@@ -3,16 +3,14 @@ package io.peter.wrapwrangler.assets.actors;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 import io.peter.wrapwrangler.assets.AssetManager;
-import io.peter.wrapwrangler.screens.FirstScreen;
+import io.peter.wrapwrangler.screens.Level;
 
 public class Floor {
 
@@ -22,23 +20,23 @@ public class Floor {
 
     Vector2 origin;
 
-
+    //Can use for ANY static object! Pass in the texture, generates static object.
     public Floor(Vector2 origin, World world) {
         this.origin = origin;
 
         sprite = new Sprite(AssetManager.floorTex);
         sprite.setOriginCenter();
         sprite.setPosition(origin.x, origin.y);
-        sprite.setSize(480/FirstScreen.PPM, 70/FirstScreen.PPM);
+        sprite.setSize(AssetManager.floorTex.getWidth(), AssetManager.floorTex.getHeight());
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.StaticBody;
-        bodyDef.position.set(sprite.getX()/FirstScreen.PPM, sprite.getY()/FirstScreen.PPM);
+        bodyDef.position.set(sprite.getX()/ Level.PPM, sprite.getY()/ Level.PPM);
 
         body = world.createBody(bodyDef);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox((sprite.getWidth()/2), (sprite.getHeight()/2));
+        shape.setAsBox(((sprite.getWidth() - 8) /2), ((sprite.getHeight() -8) /2));
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
@@ -54,8 +52,8 @@ public class Floor {
     }
 
     public void render(SpriteBatch spriteBatch){
-        Sprite bodySprite = (Sprite) body.getUserData();
-        bodySprite.setPosition((body.getPosition().x - sprite.getWidth()/2), (body.getPosition().y - sprite.getHeight()/2));
-        bodySprite.draw(spriteBatch);
+        //Sprite bodySprite = (Sprite) body.getUserData();
+        sprite.setPosition((body.getPosition().x - sprite.getWidth()/2), (body.getPosition().y - sprite.getHeight()/2));
+        sprite.draw(spriteBatch);
     }
 }
