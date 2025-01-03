575ac04374b3394e37ec285b39e44d3545f48f36
Abulaf1a <peterrazzell688@gmail.com>
2025-01-03 10:40:24 +0000

    feat: implement contact logic for spike and wrap

--- core/src/main/java/io/peter/wrapwrangler/physics/Physics.java
+++ core/src/main/java/io/peter/wrapwrangler/physics/Physics.java
@@ -1,37 +1,40 @@
 package io.peter.wrapwrangler.physics;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.Manifold;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-import io.peter.wrapwrangler.assets.AssetManager;
-import io.peter.wrapwrangler.assets.actors.Floor;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.peter.wrapwrangler.assets.actors.Player;
-import io.peter.wrapwrangler.screens.FirstScreen;
+import io.peter.wrapwrangler.assets.actors.Spike;
+import io.peter.wrapwrangler.assets.actors.Wrap;
+import io.peter.wrapwrangler.screens.Level;
+import io.peter.wrapwrangler.ui.UI;
 
 public class Physics {
     Vector2 gravityDir;
     float GRAVITY = -98f;
     World world;
     Box2DDebugRenderer debugRenderer;
-    FirstScreen firstScreen;
+    Level level;
 
-    public void physicsSetup(FirstScreen firstScreen, float PPM) {
+    static List<Wrap> subscribers;
+
+    public void physicsSetup(Level level, float PPM) {
+
+        subscribers = new ArrayList<>();
 
         Box2D.init();
 
-        this.firstScreen = firstScreen;
+        this.level = level;
 
         gravityDir = new Vector2(0, GRAVITY);
 
@@ -40,18 +43,46 @@ public class Physics {
         world.setContactListener(new ContactListener() {
             @Override
             public void beginContact(Contact contact) {
-                System.out.println(contact.getFixtureA() + " has contacted" + contact.getFixtureB());
-                firstScreen.getPlayer().isJumping = false;
-                firstScreen.getPlayer().isOnFloor = true;
+                System.out.println(contact.getFixtureA().getBody().getType() + " has contacted" + contact.getFixtureB().getBody().getType());
+                if(contact.getFixtureA().isSensor() || contact.getFixtureB().isSensor()){
+                    //wrap is always FixtureB
+                    System.out.println("player has collided with wrap");
+
+                    Object contacted = contact.getFixtureB().getBody().getUserData();
+
+                    if(contacted instanceof Wrap ){
+                        Wrap contactedWrap = (Wrap) contact.getFixtureB().getBody().getUserData();
+                        for(Wrap wrap : subscribers){
+                            if(wrap.hashCode() == contactedWrap.hashCode()){
+                                if(!wrap.isCollected()){
+                                    UI.updateScore();
+                                    wrap.dispose();
+                                }
+                            }
+                        }
+                    }
+                    else if(contacted instanceof Spike){
+                        Spike contactedSpike = (Spike) contact.getFixtureB().getBody().getUserData();
+
+
+                        contactedSpike.onHit(level.getPlayer());
+
+                        }
+
+
+                }
+                else{
+                    level.getPlayer().isJumping = false;
+                    level.getPlayer().isOnFloor = true;
+                }
             }
 
             @Override
             public void endContact(Contact contact) {
-                System.out.println(contact.getFixtureA() + " has stopped contacting" + contact.getFixtureB());
-                if(firstScreen.getPlayer().isJumping){
-                    firstScreen.getPlayer().isOnFloor = false;
+                System.out.println(contact.getFixtureA().getBody().getType() + " has stopped contacting" + contact.getFixtureB().getBody().getType());
+                if(level.getPlayer().isJumping){
+                    level.getPlayer().isOnFloor = false;
                 }
-
             }
 
             @Override
@@ -76,11 +107,15 @@ public class Physics {
         //velocity and position iterations are apparently to do with physics simulation accuracy
         //higher = more accurate and slower to process.
         world.step(1/60f, 6, 2);
-        debugRenderer.render(world, firstScreen.getViewport().getCamera().combined);
+        debugRenderer.render(world, Level.viewport.getCamera().combined);
     }
 
     public World getWorld(){
         if(world != null)  return world;
         return new World(new Vector2(0, GRAVITY), true);
+    }
+
+    public static void addSubscriber(Wrap wrap){
+        subscribers.add(wrap);
     }
 }
