d594487afc2e6a89c6d0358ba884135fa5bb1b4e
Abulaf1a <peterrazzell688@gmail.com>
2025-01-03 10:24:47 +0000

    feat: updated assets images and asset manager class

--- core/src/main/java/io/peter/wrapwrangler/assets/AssetManager.java
+++ core/src/main/java/io/peter/wrapwrangler/assets/AssetManager.java
@@ -4,10 +4,15 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class AssetManager {
 
-    public static Texture backgroundTex = new Texture("com/badlogic/gdx/sprites/Background_2.png");
+    //public static Texture backgroundTex = new Texture("com/badlogic/gdx/sprites/Background_2.png");
 
     public static Texture floorTex = new Texture("com/badlogic/gdx/sprites/dirt_tile.png");
 
-    public static Texture spriteSheet = new Texture("com/badlogic/gdx/sprites/cowboy_anim.png");
+    public static Texture cowboySpriteSheet = new Texture("com/badlogic/gdx/sprites/cowboy_anim.png");
 
+    public static Texture wrapSpriteSheet = new Texture("com/badlogic/gdx/sprites/wrap.png");
+
+    public static Texture spikeSprite = new Texture("com/badlogic/gdx/sprites/spikes.png");
+
+    public static Texture baddieSprite = new Texture("com/badlogic/gdx/sprites/alt_cowboy_single.png");
 }
